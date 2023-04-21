1d76d3c1a7d6d06ee7080b7eae5897b22da56dc4
mcallado <ma.collado@alumnos.upm.es>
2023-04-21 13:49:36 +0200

    esqueleto

--- Trabajo.c
+++ Trabajo.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
-    int b = 10;
-    int p;
-    int k = 20;
-    p = k*b;
-    char frase[] = "Hola que tal";
+    
+    
     return 0;
 }
