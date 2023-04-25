a5c37995f83dd95344cb92aeb8d3b1d5f5040ef5
maarioo13 <m.hgonzalez@alumnos.upm.es>
2023-04-25 18:49:59 +0200

    prueba

--- Trabajo.c
+++ Trabajo.c
@@ -2,7 +2,19 @@
 
 int main()
 {
-    
+    FILE *pf;
+    pf = fopen("generacion_21_22.txt", "r");
+
+    if (pf == NULL)
+    {
+        printf("Error \n");
+        return -1;
+    }
+    else
+    {
+        printf("Abierto \n");
+        return 0;
+    }
     
     return 0;
 }
